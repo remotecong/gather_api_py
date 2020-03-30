ae4eef206692f3fcd70deec929a58e3bff112677
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-03-30 17:51:01 -0500

    barebones results

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,16 @@ from sanic import Sanic
 from sanic.response import json
 from sanic.exceptions import ServerError
 from .owner_info import get_owner_data
+from .thatsthem import get_phone_numbers
 
 app = Sanic()
 
 @app.route('/')
 async def lookup(req):
     if 'address' in req.raw_args:
-        return json(get_owner_data(req.raw_args['address']))
+        a = req.raw_args['address']
+        owner_data = get_owner_data(a)
+        owner_data['phones'] = get_phone_numbers(a)
+        return json(owner_data)
     else:
         raise ServerError("No address query", status_code=400)
