a93dd4388c04d2dba58a318668279fe0e0ae5408
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-03-30 20:53:42 -0500

    added cache

--- app/lookup.py
+++ app/lookup.py
@@ -1,7 +1,17 @@
 from .owner_info import get_owner_data
 from .thatsthem import get_phone_numbers
+from datetime import timedelta
+import redis
+import json
+
+r = redis.Redis(host='redis')
 
 def lookup(address):
+    # try cache first
+    cache = r.get(address)
+    if cache:
+        return json.loads(cache)
+
     d = get_owner_data(address)
     phones = get_phone_numbers(address)
 
@@ -15,5 +25,7 @@ def lookup(address):
         if (d['lives_there'] and ln in n) or\
             (not d['lives_there'] and ln not in n):
             d['phones'].append(p)
+
+    r.setex(address, timedelta(hours=1), value=json.dumps(d))
 
     return d
