40514d0f2e4b3006dc7610adced4b4e6246c561e
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-07-01 22:37:31 -0500

    added do not call logic

--- app/locations_convert.py
+++ app/locations_convert.py
@@ -1,4 +1,5 @@
 """ territory helper locations converter """
+import sys
 import pandas as pd
 from mongo import add_address
 
@@ -6,12 +7,15 @@ DATA = pd.read_excel("sample.xlsx")
 
 KEYS_WE_CARE_ABOUT = ("Territory number", "Address", "Status")
 
+TARGET_TERR = sys.argv[1]
+
 ROW_COUNT = DATA.shape[0]
 if ROW_COUNT > 0:
     for r in range(ROW_COUNT):
         terr = DATA.at[r, "Territory number"]
-        doc = {
-            "address": DATA.at[r, "Address"],
-            "doNotCall": DATA.at[r, "Status"] == "Do not call",
-        }
-        add_address(terr, doc)
+        if not TARGET_TERR or terr == TARGET_TERR:
+            doc = {
+                "address": DATA.at[r, "Address"],
+                "doNotCall": DATA.at[r, "Status"] == "Do not call",
+            }
+            add_address(terr, doc)
--- app/print_territory.py
+++ app/print_territory.py
@@ -6,42 +6,65 @@ def key_residence(res):
     """ mapper for sorting residences """
     return res["address"]
 
-if len(sys.argv) != 2:
-    raise ValueError("you need to include a territory id")
-
-TERR = sys.argv[1]
-print("finding addresses for {}".format(TERR))
-
-territory = {}
-
-for doc in get_all_docs_for(TERR):
-    if "street" in doc:
-        street = doc["street"]
-        if street not in territory:
-            territory[street] = []
-        territory[street].append(doc)
+def ppp(name, address, phones, notes=None):
+    """ handles printing records """
+    if notes:
+        print("{}\t{}\t{}\t{}".format(name, address, phones, notes))
     else:
-        raise ValueError("no street?")
+        print("{}\t{}\t{}".format(name, address, phones))
 
-for street, residences in territory.items():
-    print("\n\n=========================================================\n{}".format(street))
-    residences.sort(key=key_residence)
-    for r in residences:
-        printable_addr = get_gather_address(r["address"])
-        if "ownerLivesThere" in r:
-            if "phoneNumbers" in r:
-                #   first two numbers, any more we don't track (unless some disconnected)
-                phones = list({p["number"] for p in r["phoneNumbers"]})[0:2]
-                if len(phones) > 1:
-                    phones = "\"{}\"".format("\n".join(phones))
-                elif len(phones) > 0:
-                    phones = "".join(phones)
-                else:
-                    phones = "No Number Found"
-                print("{}\t{}\t{}".format(r["name"], printable_addr, phones))
-            elif r["ownerLivesThere"]:
-                print("{}\t{}\t{}".format(r["ownerName"], printable_addr, "No Number Found"))
-            else:
-                print("Current Resident\t{}\tNo Number Found".format(printable_addr))
+class TerritoryMissingStreetException(Exception):
+    """ exception thrown when street name missing """
+
+def get_territory_docs(territory_id):
+    """ collects all docs for territory """
+    territory = {}
+    for doc in get_all_docs_for(territory_id):
+        if "street" in doc:
+            street = doc["street"]
+            if street not in territory:
+                territory[street] = []
+            territory[street].append(doc)
         else:
-            print("Unknown\t{}\tN/A".format(printable_addr))
+            raise TerritoryMissingStreetException
+    return territory.items()
+
+def print_territory(territory_id):
+    """ print territory outright """
+    print("=== Printing Territory {} ===".format(territory_id))
+
+    for street, residences in get_territory_docs(territory_id):
+        print("\n\n=========================================================\n{}\n".format(street))
+        residences.sort(key=key_residence)
+        for res in residences:
+            printable_addr = get_gather_address(res["address"])
+            dnc = "doNotCall" in res and res["doNotCall"]
+
+            # assessor missed this one
+            if "ownerLivesThere" not in res:
+                ppp("Unknown", printable_addr, "Do Not Call" if dnc else "N/A", "DNC")
+                return
+
+            name = res["name"] if "name" in res else "Current Resident"
+
+            # DO NOT CALL!
+            if dnc:
+                ppp(name, printable_addr, "Do Not Call", "DNC")
+
+            phone_numbers = res["phoneNumbers"] if "phoneNumbers" in res else []
+            # first two numbers, any more we don't track (unless some disconnected)
+            phones = list({p["number"] for p in phone_numbers})[0:2]
+
+            if len(phones) > 1:
+                phones = "\"{}\"".format("\n".join(phones))
+            elif len(phones) > 0:
+                phones = "".join(phones)
+            else:
+                phones = "No Number Found"
+            ppp(name, printable_addr, phones)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("you need to include a territory id")
+    print_territory(sys.argv[1])
