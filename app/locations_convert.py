d7620bebc9b9b7698f73ad8c77aa9c18fb1a4dbb
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-08-31 18:59:47 -0500

    added locations counter

--- app/locations_convert.py
+++ app/locations_convert.py
@@ -10,11 +10,14 @@ TARGET_TERR = sys.argv[1]
 ROW_COUNT = DATA.shape[0]
 if ROW_COUNT > 0:
     print("Loading Territory {}\n{} Locations found.".format(TARGET_TERR, ROW_COUNT))
+    added_count = 0
     for r in range(ROW_COUNT):
         terr = DATA.at[r, "Territory number"]
         if not TARGET_TERR or terr == TARGET_TERR:
+            added_count += 1
             doc = {
                 "address": DATA.at[r, "Address"],
                 "doNotCall": DATA.at[r, "Status"] == "Do not call",
             }
             add_address(terr, doc)
+    print("Added {} locations".format(added_count))
