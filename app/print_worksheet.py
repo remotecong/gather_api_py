eec323621c4afa25f5367d38e29267be3ba28fe7
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-09-19 14:54:54 -0500

    added some styles, just need dnc and letter no sign styles

--- app/print_worksheet.py
+++ app/print_worksheet.py
@@ -1,13 +1,37 @@
 """ print worksheets at a time now! """
 import sys
 from collections import namedtuple
+from copy import copy
 from datetime import datetime
 
 from openpyxl import Workbook, load_workbook
+from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
 
 from addresses import get_gather_address
 from names import pretty_print_name
 from print_territory import get_territory_docs, key_residence, phone_number_sort
+
+def add_default_styles(workbook):
+    """ plug in reusable styles """
+    border = Side(style="thin", color="000000")
+    thick_border = Side(style="thick", color="000000")
+
+    default_style = NamedStyle(name="default_style")
+    default_style.font = Font(size=15)
+    default_style.border = Border(top=border, left=border, bottom=border, right=border)
+    workbook.add_named_style(default_style)
+
+    top_row = copy(default_style)
+    top_row.name = "top_row"
+    top_row.border = Border(top=thick_border, left=border, bottom=border, right=border)
+    workbook.add_named_style(top_row)
+
+    centered = Alignment(horizontal="center")
+    for style in (default_style, top_row):
+        style = copy(style)
+        style.name = style.name + "_centered"
+        style.alignment = centered
+        workbook.add_named_style(style)
 
 
 def create_workbook():
@@ -24,35 +48,36 @@ def make_street_sheet(workbook, t_id, street):
     """ copies template and makes sure that we print everything """
     current_street_sheet = workbook.copy_worksheet(workbook["Street Name"])
     current_street_sheet.title = street
-    write_to_sheet(current_street_sheet, 0, 1, t_id)
+    write_to_sheet(current_street_sheet, 0, 1, t_id, None)
     return current_street_sheet
 
 
 COLS = [chr(65 + i) for i in range(26)]
 
-def write_to_sheet(sheet, col, row, val):
+def write_to_sheet(sheet, col, row, val, style="default_style"):
     """ write to sheet """
-    sheet["{}{}".format(COLS[col], row)] = val
+    loc = "{}{}".format(COLS[col], row)
+    sheet[loc] = val
+    if style:
+        sheet[loc].style = style
 
 
 def write_row(sheet, row, data):
     """ print a whole row at a time """
-    write_to_sheet(sheet, 0, row, data.name)
-    write_to_sheet(sheet, 1, row, data.address)
-    write_to_sheet(sheet, 2, row, data.phone)
-    if data.call1:
-        write_to_sheet(sheet, 3, row, data.call1)
-    if data.call2:
-        write_to_sheet(sheet, 4, row, data.call2)
-    if data.letter:
-        write_to_sheet(sheet, 5, row, data.letter)
-    if data.note:
-        write_to_sheet(sheet, 6, row, data.note)
+    style = "top_row" if row == 3 else "default_style"
+    centered = style + "_centered"
+    write_to_sheet(sheet, 0, row, data.name, style)
+    write_to_sheet(sheet, 1, row, data.address, style)
+    write_to_sheet(sheet, 2, row, data.phone, style)
+    write_to_sheet(sheet, 3, row, data.call1, centered)
+    write_to_sheet(sheet, 4, row, data.call2, centered)
+    write_to_sheet(sheet, 5, row, data.letter, centered)
+    write_to_sheet(sheet, 6, row, data.note, style)
     return row + 1
 
 
 ROW_FIELDS = ("name", "address", "phone", "call1", "call2", "letter", "note")
-Row = namedtuple("Row", ROW_FIELDS, defaults=(None,) * len(ROW_FIELDS))
+Row = namedtuple("Row", ROW_FIELDS, defaults=("",) * len(ROW_FIELDS))
 
 def stamp_last_updated(workbook, newest_date):
     """ stamps the last updated date on the readme page """
@@ -61,13 +86,14 @@ def stamp_last_updated(workbook, newest_date):
 
 def remove_template_sheets(workbook):
     """ cleans up template sheets """
-    for name in ("Street Name", "Street 2 Name"):
+    for name in ("Street Name",):
         workbook.remove(workbook[name])
 
 
 def print_workbook(t_id):
     """  ready! to print """
     workbook = open_workbook("template.xlsx")
+    add_default_styles(workbook)
 
     for street, residences in get_territory_docs(t_id):
         residences.sort(key=key_residence)
